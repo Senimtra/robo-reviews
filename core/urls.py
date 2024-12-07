3c861b24ab64ff76632a4ca49473ca8807efb6c7
Senimtra <senimtra@hotmail.com>
2024-12-07 22:54:47 +0100

    add routes: index, detail, review, genre

--- core/urls.py
+++ core/urls.py
@@ -4,4 +4,10 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name = 'index'),
+    # ex: /
+    path('<str:parent_asin>/', views.detail, name = 'detail'),
+    # ex: /B00XY0IVK4/reviews/
+    path('<str:parent_asin>/reviews/', views.reviews, name = 'reviews'),
+    # ex: /B00XY0IVK4/genre/
+    path('<str:parent_asin>/genre/', views.genre, name = 'genre'),
 ]
--- core/views.py
+++ core/views.py
@@ -1,4 +1,25 @@
 from django.http import HttpResponse
+from django.shortcuts import get_object_or_404, render
+
+from .models import Review
+
+
+# def detail(request, game_id):
+#     game = get_object_or_404(Game, pk=game_id)
+#     return render(request, "detail.html", {"game": game})
+
 
 def index(request):
-    return HttpResponse('Backend: online.')
+    latest_reviews_list = Review.objects.order_by("-timestamp")[:5]
+    context = {"latest_reviews_list": latest_reviews_list}
+    return render(request, "index.html", context)
+
+def detail(request, parent_asin):
+    return HttpResponse("You're looking at game %s." % parent_asin)
+
+def reviews(request, parent_asin):
+    response = "You're looking at the reviews for game %s."
+    return HttpResponse(response % parent_asin)
+
+def genre(request, parent_asin):
+    return HttpResponse("You're looking at the genre of game %s." % parent_asin)
